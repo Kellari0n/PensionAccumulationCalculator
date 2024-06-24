001e8a234fc8c82890a458dc8cc20c35a55e871a
Фасхутдинов Михаил <misha21fm@gmail.com>
2024-06-24 10:01:34 +0300

    Структура проекта

--- PensionAccumulationCalculator/Entities/Client.cs
+++ PensionAccumulationCalculator/Entities/Client.cs
@@ -1,7 +1,7 @@
 ﻿namespace PensionAccumulationCalculator.Entities {
     internal class Client {
         public int User_id { get; set; }
-        public string Second_name { get; set; }
+        public string Second_name { get; set; } 
         public string First_name { get; set; }
         public string Last_name { get; set; }
         public string Email { get; set; }  
--- PensionAccumulationCalculator/Entities/Individual_pencion_coefficient_accumulation.cs
+++ PensionAccumulationCalculator/Entities/Individual_pencion_coefficient_accumulation.cs
@@ -2,6 +2,8 @@
     internal class Individual_pencion_coefficient_accumulation {
         public int Accumulation_exp_id { get; set; }
         public int User_id { get; set; }
+        public int Record_id { get; set; } 
+        public string Record_name { get; set; }
         public float Individual_pencion_coefficient { get; set; } 
         public int Year { get; set; }
     }
