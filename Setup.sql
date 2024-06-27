f101afd45c7fbb56a161ac652dbf905436e9ccff
Георгий Филиппов <filippovgg2@gmail.com>
2024-06-27 17:51:48 +0300

    Добавил процедуры CRUD для оставшихся таблиц

--- Setup.sql
+++ Setup.sql
@@ -14,6 +14,8 @@ CREATE TABLE Ref_tables (
 	table_name NVARCHAR(50)
 );
 
+SET IDENTITY_INSERT dbo.Ref_tables ON;  
+
 CREATE TABLE Clients (
 	User_id INT PRIMARY KEY IDENTITY(1,1),
 	Second_name NVARCHAR(30),
@@ -44,7 +46,7 @@ CREATE TABLE Work_records (
 	FOREIGN KEY (Year) REFERENCES Ref_coefficients_cost_by_year(Year)
 );
 
-CREATE TABLE Insurance_record (
+CREATE TABLE Insurance_records (
 	Insurance_exp_id INT PRIMARY KEY IDENTITY(1,1),
 	User_id INT,
 	Individual_pension_coefficient DECIMAL(6,2),
@@ -53,7 +55,7 @@ CREATE TABLE Insurance_record (
 	FOREIGN KEY (Year) REFERENCES Ref_coefficients_cost_by_year(Year)
 );
 
-CREATE TABLE Military_record (
+CREATE TABLE Military_records (
 	Military_exp_id INT PRIMARY KEY IDENTITY(1,1),
 	User_id INT,
 	Individual_pension_coefficient DECIMAL(6,2),
@@ -81,8 +83,21 @@ CREATE TABLE Error_logs (
 	Details NVARCHAR(200),
 	FOREIGN KEY (Source_table_id) REFERENCES Ref_tables(table_id),
 );
+GO
+
+INSERT Ref_tables(table_id, table_name)
+VALUES
+	(1, 'Users'),
+	(2, 'Clients'),
+	(3, 'Work_records'),
+	(4, 'Insurance_record'),
+	(5, 'Military_record'),
+	(6, 'Individual_pencion_coefficient_accumulation'),
+	(7, 'Ref_coefficients_cost_by_year');
 
 GO
+
+-- USERS
 
 CREATE PROCEDURE dbo.CreateUser
 	@login NVARCHAR(50),
@@ -100,8 +115,8 @@ BEGIN
 		COMMIT TRAN
 	END TRY
 	BEGIN CATCH
-		--INSERT Error_logs(Error_datetime, Source_table_id, Details)
-		--VALUES (GETDATE(), ,'');
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 1, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
 
 		DBCC CHECKIDENT ('Users', RESEED);
 		DBCC CHECKIDENT ('Clients', RESEED);
@@ -109,7 +124,7 @@ BEGIN
 		ROLLBACK TRANSACTION;
 	END CATCH
 END
-GO 
+GO
 
 CREATE PROCEDURE dbo.GetUserById
 	@id INT
@@ -117,16 +132,13 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 	BEGIN TRY
-		BEGIN TRAN
-			SELECT *
-			FROM Users
-			WHERE User_id = @id;
-		COMMIT TRAN
+		SELECT *
+		FROM Users
+		WHERE User_id = @id;
 	END TRY
 	BEGIN CATCH
-		-- ERROR LOG
-
-		ROLLBACK TRANSACTION;
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 1, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
 	END CATCH
 END
 GO
@@ -135,15 +147,12 @@ CREATE PROCEDURE dbo.GetAllUsers AS
 BEGIN
 	SET NOCOUNT ON;
 	BEGIN TRY
-		BEGIN TRAN
-			SELECT *
-			FROM Users;
-		COMMIT TRAN
+		SELECT *
+		FROM Users;
 	END TRY
 	BEGIN CATCH
-		-- ERROR LOG
-
-		ROLLBACK TRANSACTION;
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 1, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
 	END CATCH
 END
 GO
@@ -156,16 +165,13 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 	BEGIN TRY
-		BEGIN TRAN
-			UPDATE Users
-			SET Login = @login, Password = @password
-			WHERE User_id = @id;
-		COMMIT TRAN
+		UPDATE Users
+		SET Login = @login, Password = @password
+		WHERE User_id = @id;
 	END TRY
 	BEGIN CATCH
-		-- ERROR LOG
-
-		ROLLBACK TRANSACTION;
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 1, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
 	END CATCH
 END
 GO
@@ -185,12 +191,524 @@ BEGIN
 		COMMIT TRAN
 	END TRY
 	BEGIN CATCH
-		-- ERROR LOG
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 1, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
 
 		DBCC CHECKIDENT ('Users', RESEED);
 		DBCC CHECKIDENT ('Clients', RESEED);
 
 		ROLLBACK TRANSACTION;
+	END CATCH
+END
+GO
+
+-- CLIENTS
+
+CREATE PROCEDURE dbo.GetClientById
+	@id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Clients
+		WHERE User_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 2, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.GetAllClients AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Clients;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 2, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.UpdateClient 
+	@id INT,
+	@second_name NVARCHAR(30),
+	@first_name NVARCHAR(30),
+	@last_name NVARCHAR(30),
+	@phone_number NVARCHAR(15),
+	@email NVARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		UPDATE Clients
+		SET Second_name = @second_name, First_name = @first_name, Last_name = @last_name, Phone_number = @phone_number, Email = @email
+		WHERE User_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 2, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+--WORK_RECORDS
+
+CREATE PROCEDURE dbo.CreateWorkRecord
+	@user_id INT,
+	@individual_pension_coefficient DECIMAL(6, 2),
+	@year INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		INSERT Work_records(User_id, Individual_pension_coefficient, Year)
+		VALUES (@user_id, @individual_pension_coefficient, @year);
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 3, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+
+		DBCC CHECKIDENT ('Work_records', RESEED);
+	END CATCH
+END
+GO 
+
+CREATE PROCEDURE dbo.GetWorkRecordById
+	@id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Work_records
+		WHERE Work_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 3, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.GetAllWorkRecords AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Work_records;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 3, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.UpdateWorkRecord
+	@id INT,
+	@user_id INT,
+	@individual_pension_coefficient DECIMAL(6, 2),
+	@year INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		UPDATE Work_records
+		SET User_id = @user_id, Individual_pension_coefficient = @individual_pension_coefficient, Year = @year
+		WHERE Work_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 3, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.DeleteWorkRecord
+	@id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+			DELETE FROM Work_records
+			WHERE Work_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 3, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+
+		DBCC CHECKIDENT ('Work_records', RESEED);
+	END CATCH
+END
+GO
+
+--INSURANCE_RECORDS
+
+CREATE PROCEDURE dbo.CreateInsuranceRecord
+	@user_id INT,
+	@individual_pension_coefficient DECIMAL(6, 2),
+	@year INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		INSERT Insurance_records(User_id, Individual_pension_coefficient, Year)
+		VALUES (@user_id, @individual_pension_coefficient, @year);
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 4, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+
+		DBCC CHECKIDENT ('Insurance_records', RESEED);
+	END CATCH
+END
+GO 
+
+CREATE PROCEDURE dbo.GetInsuranceRecordById
+	@id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Insurance_records
+		WHERE Insurance_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 4, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.GetAllInsuranceRecords AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Insurance_records;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 4, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.UpdateInsuranceRecord
+	@id INT,
+	@user_id INT,
+	@individual_pension_coefficient DECIMAL(6, 2),
+	@year INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		UPDATE Insurance_records
+		SET User_id = @user_id, Individual_pension_coefficient = @individual_pension_coefficient, Year = @year
+		WHERE Insurance_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 4, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.DeleteInsuranceRecord
+	@id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+			DELETE FROM Insurance_records
+			WHERE Insurance_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 4, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+
+		DBCC CHECKIDENT ('Work_records', RESEED);
+	END CATCH
+END
+GO
+
+-- MILITARY_RECORDS
+
+CREATE PROCEDURE dbo.CreateMilitaryRecord
+	@user_id INT,
+	@individual_pension_coefficient DECIMAL(6, 2),
+	@year INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		INSERT Military_records(User_id, Individual_pension_coefficient, Year)
+		VALUES (@user_id, @individual_pension_coefficient, @year);
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 5, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+
+		DBCC CHECKIDENT ('Military_records', RESEED);
+	END CATCH
+END
+GO 
+
+CREATE PROCEDURE dbo.GetMilitaryRecordById
+	@id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Military_records
+		WHERE Military_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 5, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.GetAllMilitaryRecords AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Military_records;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 5, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.UpdateMilitaryRecord
+	@id INT,
+	@user_id INT,
+	@individual_pension_coefficient DECIMAL(6, 2),
+	@year INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		UPDATE Military_records
+		SET User_id = @user_id, Individual_pension_coefficient = @individual_pension_coefficient, Year = @year
+		WHERE Military_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 5, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.DeleteMilitaryRecord
+	@id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+			DELETE FROM Military_records
+			WHERE Military_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 5, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+
+		DBCC CHECKIDENT ('Military_records', RESEED);
+	END CATCH
+END
+GO
+
+-- INDIVIDUAL_PENCION_COEFFICIENT_ACCUMULATION
+
+CREATE PROCEDURE dbo.CreateIndividualPencionCoefficientAccumulation
+	@user_id INT,
+	@individual_pension_coefficient DECIMAL(6, 2),
+	@year INT,
+	@record_id INT,
+	@table_id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		INSERT Individual_pencion_coefficient_accumulation(User_id, Individual_pension_coefficient, Year, Record_id, Table_id)
+		VALUES (@user_id, @individual_pension_coefficient, @year, @record_id, @table_id);
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 6, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+
+		DBCC CHECKIDENT ('Individual_pencion_coefficient_accumulation', RESEED);
+	END CATCH
+END
+GO 
+
+CREATE PROCEDURE dbo.GetIndividualPencionCoefficientAccumulationById
+	@id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Individual_pencion_coefficient_accumulation
+		WHERE Accumulation_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 6, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.GetAllIndividualPencionCoefficientAccumulations AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Individual_pencion_coefficient_accumulation;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 6, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.UpdateIndividualPencionCoefficientAccumulation
+	@id INT,
+	@user_id INT,
+	@individual_pension_coefficient DECIMAL(6, 2),
+	@year INT,
+	@record_id INT,
+	@table_id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		UPDATE Individual_pencion_coefficient_accumulation
+		SET User_id = @user_id, Individual_pension_coefficient = @individual_pension_coefficient, Year = @year, Record_id = @record_id, Table_id = @table_id
+		WHERE Accumulation_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 6, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.DeleteIndividualPencionCoefficientAccumulation
+	@id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+			DELETE FROM Individual_pencion_coefficient_accumulation
+			WHERE Accumulation_exp_id = @id;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 6, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+
+		DBCC CHECKIDENT ('Individual_pencion_coefficient_accumulation', RESEED);
+	END CATCH
+END
+GO
+
+-- REF_COEFFICIENTS_COST_BY_YEAR
+
+CREATE PROCEDURE dbo.CreateRefCoefficientCostByYear 
+	@year INT,
+	@cost DECIMAL(6,2)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		INSERT Ref_coefficients_cost_by_year(Year, Cost)
+		VALUES (@year, @cost);
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 7, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+
+		DBCC CHECKIDENT ('Ref_coefficients_cost_by_year', RESEED);
+	END CATCH
+END
+GO 
+
+CREATE PROCEDURE dbo.GetRefCoefficientCostByYear
+	@year INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Ref_coefficients_cost_by_year
+		WHERE Year = @year;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 7, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.GetAllRefCoefficientsCostByYear AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SELECT *
+		FROM Ref_coefficients_cost_by_year;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 7, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.UpdateRefCoefficientCostByYear
+	@year INT,
+	@cost DECIMAL(6,2)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		UPDATE Ref_coefficients_cost_by_year
+		SET Cost = @cost 
+		WHERE Year = @year;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 7, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE dbo.DeleteRefCoefficientCostByYear
+	@year INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+			DELETE FROM Ref_coefficients_cost_by_year
+			WHERE Year = @year;
+	END TRY
+	BEGIN CATCH
+		INSERT Error_logs(Error_datetime, Source_table_id, Details)
+		VALUES (GETDATE(), 7, 'ERROR ' + ERROR_NUMBER() + ': ' + ERROR_MESSAGE());
+
+		DBCC CHECKIDENT ('Ref_coefficients_cost_by_year', RESEED);
 	END CATCH
 END
 GO
